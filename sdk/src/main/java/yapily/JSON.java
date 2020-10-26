8ba88d950eccd66f4a39d5a749600aa21630c5e0
Jenkins <systems@yapily.com>
2020-10-26 16:20:32 +0000

    latest version

--- sdk/src/main/java/yapily/JSON.java
+++ sdk/src/main/java/yapily/JSON.java
@@ -8,7 +8,7 @@ import java.text.DateFormat;
 
 import javax.ws.rs.ext.ContextResolver;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-23T10:11:10.870Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-26T16:19:35.564Z")
 public class JSON implements ContextResolver<ObjectMapper> {
   private ObjectMapper mapper;
 
--- sdk/src/main/java/yapily/sdk/IdentityApi.java
+++ sdk/src/main/java/yapily/sdk/IdentityApi.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-23T10:11:10.870Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-26T16:19:35.564Z")
 public class IdentityApi {
   private ApiClient apiClient;
 
