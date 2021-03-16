1dbc607cf13b6a787875ceef5fd04eb446500fac
Jenkins <systems@yapily.com>
2021-03-16 18:29:40 +0000

    latest version

--- sdk/build.sbt
+++ sdk/build.sbt
@@ -2,7 +2,7 @@ lazy val root = (project in file(".")).
   settings(
     organization := "yapily",
     name := "yapily-sdk",
-    version := "0.0.317",
+    version := "0.0.318",
     scalaVersion := "2.11.4",
     scalacOptions ++= Seq("-feature"),
     javacOptions in compile ++= Seq("-Xlint:deprecation"),
--- sdk/src/main/java/yapily/sdk/EmbeddedPaymentsApi.java
+++ sdk/src/main/java/yapily/sdk/EmbeddedPaymentsApi.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-15T18:20:58.935Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-16T18:28:41.926Z")
 public class EmbeddedPaymentsApi {
   private ApiClient apiClient;
 
