88584f0e34e9daa8f4871695d78244c7e1628f3c
Jenkins <systems@yapily.com>
2021-03-25 19:48:51 +0000

    latest version

--- sdk/src/main/java/yapily/ApiClient.java
+++ sdk/src/main/java/yapily/ApiClient.java
@@ -50,7 +50,7 @@ import yapily.auth.HttpBasicAuth;
 import yapily.auth.ApiKeyAuth;
 import yapily.auth.OAuth;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-24T14:19:35.959Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-25T19:47:46.583Z")
 public class ApiClient {
   protected Map<String, String> defaultHeaderMap = new HashMap<String, String>();
   protected String basePath = "https://api.yapily.com";
@@ -76,7 +76,7 @@ public class ApiClient {
     this.dateFormat = new RFC3339DateFormat();
 
     // Set default User-Agent.
-    setUserAgent("Swagger-Codegen/0.0.322/java");
+    setUserAgent("Swagger-Codegen/0.0.323/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
--- sdk/src/main/java/yapily/sdk/InstitutionsOpenDataApi.java
+++ sdk/src/main/java/yapily/sdk/InstitutionsOpenDataApi.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-24T14:19:35.959Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-03-25T19:47:46.583Z")
 public class InstitutionsOpenDataApi {
   private ApiClient apiClient;
 
