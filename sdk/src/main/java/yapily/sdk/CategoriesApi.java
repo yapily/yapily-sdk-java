b76770881c489bb0d95936487610dc8f715e253d
Jenkins <systems@yapily.com>
2021-06-14 17:37:34 +0000

    latest version

--- sdk/src/main/java/yapily/sdk/CategoriesApi.java
+++ sdk/src/main/java/yapily/sdk/CategoriesApi.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-06-10T10:19:56.418Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-06-14T17:36:30.730Z")
 public class CategoriesApi {
   private ApiClient apiClient;
 
