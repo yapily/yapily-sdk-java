a432a1d8dc1bb6078ec3d7b33079f9c00725edd0
Jenkins <systems@yapily.com>
2021-08-10 15:05:01 +0000

    latest version

--- sdk/src/main/java/yapily/sdk/IdentityApi.java
+++ sdk/src/main/java/yapily/sdk/IdentityApi.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-08-10T09:12:19.417Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-08-10T15:02:34.440Z")
 public class IdentityApi {
   private ApiClient apiClient;
 
